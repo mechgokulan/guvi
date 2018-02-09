781e16af4276372a0330a8a919f16141e1c513d7
savigokul <35652723+mechgokulan@users.noreply.github.com>
2018-02-09 16:07:48 +0530

    Update gokulsa.c

--- gokulsa.c
+++ gokulsa.c
@@ -1,16 +1,23 @@
-#include<iostream.h>
-#include<conio.h>
+#include <stdio.h>
 int main()
 {
-int n;
-printf("enter the integer");
-scanf("%d",&n);
-if(n%2==0)
-{
-printf("given the number is palindrome");
+   int n=0,num,temp=0,sum=0;
+   scanf("%d",&num);
+   n=num;
+   while(n!=0)
+   {
+       temp=n%10;
+       sum=(sum*10)+temp;
+       n=n/10;
+   }
+   if(sum==num)
+   {
+       printf("palindrome");
+   }
+   else
+   {
+       printf("not a palindrome");
+   }
+   return 0;
 }
-else
-{
-printf("given the number is not palindrome");
-return 0;
-}
+
