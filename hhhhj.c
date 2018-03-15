53adc09dec728ebc453be4961ba29eded8d4942b
savigokul <35652723+mechgokulan@users.noreply.github.com>
2018-03-15 16:12:00 +0530

    Create hhhhj.c

---
+++ hhhhj.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+char str[50],i,c=0;
+printf("enter a character");
+gets(str);
+for(i=0;str[i]!='\0';i++)
+{
+if(str[i]>'a' &&str[i]<'z')
+{
+c++;
+}
+}
+printf("%d",c);
+}
