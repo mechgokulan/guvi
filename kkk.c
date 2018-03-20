bd5b4214f4aea9917d4b88badddbac180b3e5f17
savigokul <35652723+mechgokulan@users.noreply.github.com>
2018-03-20 16:12:14 +0530

    Create kkk.c

---
+++ kkk.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+char a[1000];
+int i,count=0;
+printf("enter the string\n");
+scanf("%[^n]s",&a)
+for(i=0;a[i]!='\0';i++)
+{
+  if(a[i]='')
+  {
+    count++;
+  }
+  printf("%d",count);
+}
+  
